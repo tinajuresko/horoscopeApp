da26d6c0f06536e8d107a06ab9b411db3974884c
tinajuresko <tina.juresko16@gmail.com>
2024-12-27 14:27:40 +0100

    Initial Commit

---
+++ horoscopeApp/horoscopeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  horoscopeAppApp.swift
+//  horoscopeApp
+//
+//  Created by Tina Jureško on 27.12.2024..
+//
+
+import SwiftUI
+
+@main
+struct horoscopeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ horoscopeAppUITests/horoscopeAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  horoscopeAppUITestsLaunchTests.swift
+//  horoscopeAppUITests
+//
+//  Created by Tina Jureško on 27.12.2024..
+//
+
+import XCTest
+
+final class horoscopeAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
